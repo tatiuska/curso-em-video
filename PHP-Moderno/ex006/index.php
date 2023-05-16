ef2b7fe47b76121d8725d2b7f1c01dfe12499f89
tatiuska <tfweska@gmail.com>
2023-05-16 15:28:47 -0300

    Exercício 006 (Expressões Artiméticas Compostas)

---
+++ PHP-Moderno/ex006/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Expressões Aritméticas Compostas</title>
+</head>
+<body>
+    <h1>Exemplo:</h1>
+    <p>50 / 2 + 3 ** 2</p>
+    <?php 
+        $res = 50 / 2 + 3 ** 2;
+        echo "O resultado é $res";
+    ?>
+</body>
+</html>
