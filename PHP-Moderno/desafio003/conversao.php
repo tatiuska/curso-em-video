85ec51e9d880ed267d0547878c44e439a3e098b2
tatiuska <tfweska@gmail.com>
2023-05-29 23:10:04 -0300

    finalização Desafio 003: Conversor de Moedas Simples

--- PHP-Moderno/desafio003/conversao.php
+++ PHP-Moderno/desafio003/conversao.php
@@ -10,24 +10,28 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <?php 
-        //Cotação consultada pelo Google
-        $cotacao = 4.99;
+    <main>
+        <h1>Conversor de Moedas</h1>
+        <?php 
+            //Cotação consultada pelo Google
+            $cotacao = 4.99;
 
-        //Quantos R$ você têm?
-        $real = 1000;
+            //Quantos R$ você têm?
+            $real = $_GET["din"] ?? 0;
 
-        //Equivalência em U$
-        $dolar = $real / $cotacao;
+            //Equivalência em U$
+            $dolar = $real / $cotacao;
 
-        //Mostrar o resultado - formato mais traidicional
-        //echo "Seus R\$" . number_format($real, 2, ",", ".") . " equivalem a US\$" . number_format($dolar, 2, ",", ".");
+            //Mostrar o resultado - formato mais traidicional
+            //echo "Seus R\$" . number_format($real, 2, ",", ".") . " equivalem a US\$" . number_format($dolar, 2, ",", ".");
 
-        //Mostrar o resultado - formato mais profissional
-        //Formatação de moedas com internacionalização
-        //Biblioteca intl - desabilitada por padrão no XAMPP
-        $padrao = numfmt_create("pt-BR", NumberFormatter::CURRENCY);
-        echo "Seus " . numfmt_format_currency($padrao, $real, "BRL") . " equivalem a " . numfmt_format_currency($padrao, $dolar, "USD");
-    ?>
+            //Mostrar o resultado - formato mais profissional
+            //Formatação de moedas com internacionalização
+            //Biblioteca intl - desabilitada por padrão no XAMPP
+            $padrao = numfmt_create("pt-BR", NumberFormatter::CURRENCY);
+            echo "<p>Seus " . numfmt_format_currency($padrao, $real, "BRL") . " equivalem a <strong>" . numfmt_format_currency($padrao, $dolar, "USD") . "</strong></p>";
+        ?>
+        <button onclick="javascript:history.go(-1)"><<< Voltar</button>
+    </main>
 </body>
 </html>
