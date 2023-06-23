d5064f640a719b9a5d7fbbae4676a5e6c88fc630
tatiuska <tfweska@gmail.com>
2023-06-23 19:44:53 -0300

    novo cadastro com constraints (Aula #04)

---
+++ MySQL/novo_cadastro.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE cadastro
+DEFAULT CHARACTER SET utf8mb4
+DEFAULT COLLATE uft8mb4_900_ai_ci;
+
+CREATE TABLE `pessoas` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(30) NOT NULL,
+  `nascimento` date DEFAULT NULL,
+  `sexo` enum('M','F') DEFAULT NULL,
+  `peso` decimal(5,2) DEFAULT NULL,
+  `altura` decimal(3,2) DEFAULT NULL,
+  `nacionalidade` varchar(20) DEFAULT 'Brasil',
+  PRIMARY KEY (`id`)
+) DEFAULT CHARSET = utf8mb4;
