7c76eabf977599c0f9e5966c6385c2d1caeff237
tatiuska <tfweska@gmail.com>
2023-06-25 22:02:29 -0300

    Inserção de mais indivíduos no banco de dados (Aula 05)

--- MySQL/A05_cadastro.sql
+++ MySQL/A05_cadastro.sql
@@ -24,10 +24,15 @@ SELECT * FROM `pessoas`; -- Comando que mostra a tabela com os dados inseridos.
 INSERT INTO `pessoas`(`nome`, `nascimento`, `sexo`, `peso`, `altura`, `nacionalidade`)
 VALUES('Maria', '1999-12-30', 'F', '55.2', '1.65', 'Portugal');
 
-SELECT * FROM `pessoas`;
-
 -- Usando DEFAULT para ter o id incluído nas colunas da tabela. Como é um valor que se auto-incrementa, não é obrigatório mencioná-lo na entrada de dados, mas se quiser, pode.
 INSERT INTO `pessoas`(`id`, `nome`, `nascimento`, `sexo`, `peso`, `altura`, `nacionalidade`)
 VALUES(DEFAULT, 'Creuza', '1920-07-23', 'F', '50.3', '1.62', DEFAULT);
 
-SELECT * FROM `pessoas`;
+-- Quando os dados são inseridos na mesma ordem dos campos da tabela, não é preciso citá-los no início do comando.
+INSERT INTO `pessoas` VALUES (DEFAULT, 'Adalgiza', '1930-11-02', 'F', '63.4', '1.76', 'Irlanda');
+
+-- Usando o comando INSERT INTO para inserir vários dados no mesmo comando.
+INSERT INTO `pessoas` VALUES 
+(DEFAULT, 'Ana', '1975-12-18', 'F', '54.1', '1.55', 'EUA'),
+(DEFAULT, 'Pedro', '2000-03-15', 'M', '76.8', '1.85', DEFAULT),
+(DEFAULT, 'Roberta', '1998-04-09', 'F', '60.8', '1.74', 'Portugal');
